ce3c008ae54e24da2f65fea8792ce4aa4d2c4fee
Russell Gordon <rgordon@lcs.on.ca>
2024-05-30 15:57:19 -0400

    Initial Commit

---
+++ SwipeDirectionExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeDirectionExample
+//
+//  Created by Russell Gordon on 2024-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeDirectionExample/SwipeDirectionExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeDirectionExampleApp.swift
+//  SwipeDirectionExample
+//
+//  Created by Russell Gordon on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct SwipeDirectionExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
