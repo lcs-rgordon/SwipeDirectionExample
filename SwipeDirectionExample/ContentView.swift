2cdd3b6d19fadfcbab0b4c87cd0477c8f4bc42da
Russell Gordon <rgordon@lcs.on.ca>
2024-05-30 16:41:25 -0400

    Now reports on the direction of the swipe.

--- SwipeDirectionExample/ContentView.swift
+++ SwipeDirectionExample/ContentView.swift
@@ -15,7 +15,14 @@ struct Fruit: Identifiable {
 struct ContentView: View {
     
     // MARK: Stored properties
-    @State var currentTab: Int = 1
+    
+    // Keep track of what tab we are currently on
+    @State var currentTab: Int = 0
+    
+    // Report on the swipe direction, once a swipe occurs
+    @State var swipeDirection: String = ""
+    
+    // A simple list of data (this could come from different sources in other apps)
     @State var fruits: [Fruit] = [
         Fruit(name: "bananas"),
         Fruit(name: "oranges"),
@@ -25,19 +32,44 @@ struct ContentView: View {
     
     // MARK: Computed properties
     var body: some View {
+        
         VStack {
+            
+            // Show a tab view
+            //
+            // The TabView will updated $currentTab with the tag of whatever page is currently selected
             TabView(selection: $currentTab) {
                 
+                // Iterate over the array of fruits
+                //
+                // index: holds the position of the current item in the array
+                // fruit: holds the current item in the array
                 ForEach(Array(zip(fruits.indices, fruits)), id: \.0) { index, fruit in
+                    
+                    // Display the fruit
                     Text(fruit.name)
-                        .tag(index)
+                        .tag(index) // tag this view with an integer
                 }
                 
             }
+            // Present the tab view
             .tabViewStyle(.page)
+            // Always show the dots to indicate available pages
             .indexViewStyle(.page(backgroundDisplayMode: .always))
             
             Text("Current tab index: \(currentTab)")
+            Text(swipeDirection)
+        }
+        // This is triggered whenever the current tab changes
+        .onChange(of: currentTab) { oldValue, newValue in
+            
+            // Figure out what direction the swipe was
+            if oldValue > newValue {
+                swipeDirection = "User swiped left."
+            } else {
+                swipeDirection = "User swiped right."
+            }
+            
         }
     }
 }
