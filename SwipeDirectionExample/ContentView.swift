07ce80a654d8a71c30fc7317bfd2e8d00238529e
Russell Gordon <rgordon@lcs.on.ca>
2024-05-30 16:28:54 -0400

    Paged tab view that successfully shows the tag number.

--- SwipeDirectionExample/ContentView.swift
+++ SwipeDirectionExample/ContentView.swift
@@ -7,18 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Fruit: Identifiable {
+    let id = UUID()
+    let name: String
 }
 
+struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var currentTab: Int = 1
+    @State var fruits: [Fruit] = [
+        Fruit(name: "bananas"),
+        Fruit(name: "oranges"),
+        Fruit(name: "apples"),
+        Fruit(name: "pears"),
+    ]
+    
+    // MARK: Computed properties
+    var body: some View {
+        VStack {
+            TabView(selection: $currentTab) {
+                
+                ForEach(Array(zip(fruits.indices, fruits)), id: \.0) { index, fruit in
+                    Text(fruit.name)
+                        .tag(index)
+                }
+                
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+            Text("Current tab index: \(currentTab)")
+        }
+    }
+}
 #Preview {
     ContentView()
 }
